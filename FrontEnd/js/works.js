e2d60f8e61bc50d07c0b06c64662f1ee59ab2470
Marin <marin.desloges@sfr.fr>
2023-05-25 11:42:06 +0200

    Ajout de la superposition et de la fenêtre d'édition

--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -101,17 +101,66 @@ function homepageEdit () {
         editMode.classList.add("edit-mode");
 
         let editModeBtn = document.createElement("button");
-        editModeBtn.innerText = "Mode Edition";
-      
+        editModeBtn.innerText = "Mode édition";
+
+        let editModeImg = document.createElement("i");
+        editModeImg.classList.add("fa-sharp", "fa-light", "fa-pen-to-square");
+
         let publishTheChangesBtn = document.createElement("button");
-        publishTheChangesBtn.innerText = "Publier les changements";
-      
+        publishTheChangesBtn.innerText = "publier les changements";
+
+        let projectsDiv = document.createElement("div");
+        projectsDiv.classList.add("projects-div");
+
+        let myProjects = document.querySelector(".my-projects");
+        let modify = document.createElement("button");
+        modify.classList.add("modify-btn");
+        modify.innerText = "modifier";
+        let portfolio = document.querySelector("#portfolio");
+        let gallery = document.querySelector(".gallery");
+
+        let modifyIcon = document.createElement("i");
+        modifyIcon.classList.add("fa-light", "fa-pen-to-square");
+
+        portfolio.appendChild(projectsDiv);
+        projectsDiv.appendChild(myProjects);
+        projectsDiv.appendChild(modify);
+        portfolio.insertBefore(projectsDiv, gallery);
+
+        modify.appendChild(modifyIcon)
+
         header.appendChild(editMode);
         editMode.appendChild(editModeBtn);
+        editModeBtn.appendChild(editModeImg);
         editMode.appendChild(publishTheChangesBtn);
 
         let loginBtn = document.querySelector(".login-btn");
         loginBtn.innerText = "logout";
+
+        modify = document.querySelector("button");
+
+        let modify2 = document.querySelector(".modify-btn").cloneNode(true);
+        document.querySelector(".intro-img").appendChild(modify2);
+   
+        let modify3 = document.querySelector(".modify-btn").cloneNode(true);
+        document.querySelector(".intro-txt").appendChild(modify3);
+        document.querySelector(".intro-txt").insertBefore(modify3, document.querySelector(".intro-txt-title"));
+
+        modify.addEventListener("click", function () {
+
+            let overlay = document.createElement("div");
+            overlay.setAttribute("id","overlay");
+            let body = document.querySelector("body");
+            body.appendChild(overlay);
+
+            let dialog = document.createElement("dialog");
+            dialog.classList.add("dialog");
+            body.appendChild(dialog);
+            let gallery2 = document.querySelector(".gallery").cloneNode(true);
+            dialog.appendChild(gallery2);
+
+
+    });
     }
 }
 
